ecb8023dfe52b2503bc467d3092c2d3453d09063
DimitriosMalonas <dimitriosmalonas@outlook.com>
2019-12-22 04:28:00 +0000

    Added tests package

---
+++ HangmanGame/src/tests/CommandOptsTest.java
@@ -0,0 +1,13 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.junit.Test;
+
+public class CommandOptsTest {
+
+
+}
